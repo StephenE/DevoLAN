bc17f1106ea2d3d3d4a60db193007b0e00803915
StephenE <sedevo-github@yahoo.co.uk>
2016-03-04 22:15:11 +0000

    Added the (unimplemented) game controller with unit tests

---
+++ Peril.Api.Tests/Controllers/GameControllerTest.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Peril.Api.Controllers.Api;
+using Peril.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Peril.Api.Tests.Controllers
+{
+    [TestClass]
+    public class GameControllerTest
+    {
+        [TestMethod]
+        public async Task TestGetSessions_WithNoSessions()
+        {
+            // Arrange
+            GameController controller = new GameController();
+
+            // Act
+            IEnumerable<ISession> result = await controller.GetSessions();
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(0, result.Count());
+        }
+
+        [TestMethod]
+        public async Task TestGetSessions_WithOneSession()
+        {
+            // Arrange
+            GameController controller = new GameController();
+
+            // Act
+            IEnumerable<ISession> result = await controller.GetSessions();
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(1, result.Count());
+        }
+
+        [TestMethod]
+        public async Task TestStartNewSession()
+        {
+            // Arrange
+            GameController controller = new GameController();
+
+            // Act
+            ISession result = await controller.PostStartNewSession();
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(result.GameId);
+        }
+
+        [TestMethod]
+        public async Task TestJoinSession_WithInvalidGuid()
+        {
+            // Arrange
+            GameController controller = new GameController();
+
+            // Act
+            Guid invalidGuid = new Guid("3286C8E6-B510-4F7F-AAE0-9EF827459E7E");
+            Task result = controller.PostJoinSession(invalidGuid);
+
+            // Assert
+            try
+            {
+                await result;
+                Assert.Fail("Expected exception to be thrown");
+            }
+            catch(HttpResponseException exception)
+            {
+                Assert.AreEqual(HttpStatusCode.NotFound, exception.Response.StatusCode);
+            }
+        }
+
+        [TestMethod]
+        public async Task TestJoinSession_WithValidGuid()
+        {
+            // Arrange
+            GameController controller = new GameController();
+
+            // Act
+            Guid validGuid = new Guid("68E4A0DC-BAB8-4C79-A6E9-D0A7494F3B45");
+            Task result = controller.PostJoinSession(validGuid);
+
+            // Assert
+            await result;
+        }
+
+        [TestMethod]
+        public async Task TestGetPlayers_WithInvalidGuid()
+        {
+            // Arrange
+            GameController controller = new GameController();
+
+            // Act
+            Guid invalidGuid = new Guid("3286C8E6-B510-4F7F-AAE0-9EF827459E7E");
+            Task<IEnumerable<IPlayer>> result = controller.GetPlayers(invalidGuid);
+
+            // Assert
+            try
+            {
+                await result;
+                Assert.Fail("Expected exception to be thrown");
+            }
+            catch (HttpResponseException exception)
+            {
+                Assert.AreEqual(HttpStatusCode.NotFound, exception.Response.StatusCode);
+            }
+        }
+
+        [TestMethod]
+        public async Task TestGetPlayers_WithValidGuidAndOnePlayer()
+        {
+            // Arrange
+            GameController controller = new GameController();
+
+            // Act
+            Guid validGuid = new Guid("68E4A0DC-BAB8-4C79-A6E9-D0A7494F3B45");
+            IEnumerable<IPlayer> result = await controller.GetPlayers(validGuid);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(1, result.Count());
+        }
+    }
+}
---
+++ Peril.Api/Controllers/Api/GameController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Peril.Api.Controllers.Api
+{
+    [Authorize]
+    [RoutePrefix("api/Game")]
+    public class GameController : ApiController
+    {
+        // GET /api/Game/Sessions
+        [Route("Sessions")]
+        public async Task<IEnumerable<Peril.Core.ISession>> GetSessions()
+        {
+            return null;
+        }
+
+        // POST /api/Game/StartNewGame
+        [Route("StartNewGame")]
+        public async Task<Peril.Core.ISession> PostStartNewSession()
+        {
+            return null;
+        }
+
+        // POST /api/Game/JoinGame?gameId=guid-as-string
+        [Route("JoinGame")]
+        public async Task PostJoinSession(Guid sessionId)
+        {
+            throw new HttpResponseException(new HttpResponseMessage { StatusCode = HttpStatusCode.NotFound, ReasonPhrase = "No session found with the provided Guid" });
+        }
+
+        // GET /api/Game/Players
+        [Route("Players")]
+        public async Task<IEnumerable<Peril.Core.IPlayer>> GetPlayers(Guid sessionId)
+        {
+            throw new HttpResponseException(new HttpResponseMessage { StatusCode = HttpStatusCode.NotFound, ReasonPhrase = "No session found with the provided Guid" });
+        }
+    }
+}
