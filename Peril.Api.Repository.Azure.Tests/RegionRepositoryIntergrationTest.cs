832b08d38420592cf6d2bee247bab0d952465e43
Stephen Edmonds <sedevo-github@yahoo.co.uk>
2016-03-10 13:28:23 +0000

    Started work on the Azure region repository

---
+++ Peril.Api.Repository.Azure.Tests/RegionRepositoryIntergrationTest.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Peril.Api.Repository.Azure.Tests
+{
+    [TestClass]
+    public class RegionRepositoryIntergrationTest
+    {
+        [ClassInitialize()]
+        public static void ClassInit(TestContext context)
+        {
+            CloudStorageEmulatorShepherd shepherd = new CloudStorageEmulatorShepherd();
+            shepherd.Start();
+
+            StorageAccount = CloudStorageAccount.Parse(DevelopmentStorageAccountConnectionString);
+            TableClient = StorageAccount.CreateCloudTableClient();
+            SessionTable = TableClient.GetTableReference("Sessions");
+            SessionPlayerTable = TableClient.GetTableReference("SessionPlayers");
+
+            SessionTable.DeleteIfExists();
+            SessionPlayerTable.DeleteIfExists();
+        }
+
+        [TestMethod]
+        [TestCategory("Integration")]
+        [TestCategory("RegionRepository")]
+        public async Task IntegrationTestGetRegion()
+        {
+            // Arrange
+            RegionRepository repository = new RegionRepository(DevelopmentStorageAccountConnectionString);
+            Guid dummyRegionId = new Guid("024D1E45-EF34-4AB1-840D-79229CCDE8C3");
+
+            // Act
+            IRegionData regionData = await repository.GetRegion(dummyRegionId);
+
+            // Assert
+            Assert.IsNotNull(regionData);
+        }
+
+        static private String DevelopmentStorageAccountConnectionString
+        {
+            get { return "UseDevelopmentStorage=true"; }
+        }
+
+        static private CloudStorageAccount StorageAccount { get; set; }
+        static private CloudTableClient TableClient { get; set; }
+        static private CloudTable SessionTable { get; set; }
+        static private CloudTable SessionPlayerTable { get; set; }
+    }
+}
---
+++ Peril.Api.Repository.Azure/RegionRepository.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Peril.Api.Repository.Azure
+{
+    public class RegionRepository : IRegionRepository
+    {
+        public RegionRepository(String storageConnectionString)
+        {
+            StorageAccount = CloudStorageAccount.Parse(storageConnectionString);
+            TableClient = StorageAccount.CreateCloudTableClient();
+            RegionTable = TableClient.GetTableReference("Regions");
+            RegionTable.CreateIfNotExists();
+        }
+
+        public async Task<IRegionData> GetRegion(Guid regionId)
+        {
+            throw new NotImplementedException("TODO");
+        }
+
+        private CloudStorageAccount StorageAccount { get; set; }
+        private CloudTableClient TableClient { get; set; }
+        public CloudTable RegionTable { get; set; }
+    }
+}
