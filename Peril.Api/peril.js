edcbb2287ef60c6392be20a00d7bba65489cffad
Anthony Edmonds <anthony.edmonds@live.co.uk>
2016-03-11 21:45:27 +0000

    Interactive Map
    Continuing work on completing the SVG implementation (SA, NE, EU)

--- Peril.Api/peril.js
+++ Peril.Api/peril.js
@@ -1,3 +1,29 @@
+// Board
+	// Setup Board
+		function boardSetup(){
+			// Resize Listener
+				window.addEventListener("resize", function(){resizeBoard()}, true);
+				resizeBoard();
+		}
+
+// Screen Sizing
+	// Circle Sizer
+		function resizeBoard(){
+				var x = 0;
+				var circles = document.getElementsByTagName("circle");
+				var r = 17.5;
+				
+				for(x = 0; x < circles.length; x++){
+					if(window.innerWidth < 1100 || window.innerHeight < 600){
+						r = 35;
+					} else {
+						r = 17.5;
+					}
+					
+					circles[x].setAttribute("r", r);
+				}
+		}
+
 // Fill
 	// Set Fill Colour
 		function setFillColour(target, colour){
