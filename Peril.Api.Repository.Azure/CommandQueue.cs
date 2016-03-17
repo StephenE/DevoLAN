eb554ff2a3e810b524534412fcb5099fc3a92478
Stephen Edmonds <sedevo-github@yahoo.co.uk>
2016-03-17 13:29:54 +0000

    Work in progress on command table queue. Needs testing to verify behaviour

--- Peril.Api.Repository.Azure/CommandQueue.cs
+++ Peril.Api.Repository.Azure/CommandQueue.cs
@@ -4,48 +4,101 @@ using Peril.Api.Repository.Model;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Peril.Api.Repository.Azure.Model;
 
 namespace Peril.Api.Repository.Azure
 {
     public class CommandQueue : ICommandQueue
     {
-        static public String CommandQueueTableName { get { return "CommandQueue"; } }
+        static public String CommandQueueTableNameSyntax { get { return "CommandQueue_{0}"; } }
 
         public CommandQueue(String storageConnectionString)
         {
             StorageAccount = CloudStorageAccount.Parse(storageConnectionString);
             TableClient = StorageAccount.CreateCloudTableClient();
-            CommandQueueTable = TableClient.GetTableReference(CommandQueueTableName);
-            CommandQueueTable.CreateIfNotExists();
+            
         }
 
-        public Task<Guid> DeployReinforcements(Guid sessionId, Guid phaseId, Guid targetRegion, string targetRegionEtag, uint numberOfTroops)
+        public async Task<Guid> DeployReinforcements(Guid sessionId, Guid phaseId, Guid targetRegion, String targetRegionEtag, UInt32 numberOfTroops)
         {
-            throw new NotImplementedException();
+            CloudTable commandQueueTable = GetCommandQueueTableForSession(sessionId);
+
+            // Create a new table entry
+            CommandQueueTableEntry newCommand = CommandQueueTableEntry.CreateReinforceMessage(sessionId, phaseId, targetRegion, targetRegionEtag, numberOfTroops);
+
+            // Kick off the insert operation
+            TableOperation insertOperation = TableOperation.Insert(newCommand);
+            await commandQueueTable.ExecuteAsync(insertOperation);
+
+            return newCommand.OperationId;
         }
 
-        public Task<IEnumerable<ICommandQueueMessage>> GetQueuedCommands(Guid sessionId)
+        public async Task<Guid> OrderAttack(Guid sessionId, Guid phaseId, Guid sourceRegion, String sourceRegionEtag, Guid targetRegion, UInt32 numberOfTroops)
         {
-            throw new NotImplementedException();
+            CloudTable commandQueueTable = GetCommandQueueTableForSession(sessionId);
+
+            // Create a new table entry
+            CommandQueueTableEntry newCommand = CommandQueueTableEntry.CreateAttackMessage(sessionId, phaseId, sourceRegion, sourceRegionEtag, targetRegion, numberOfTroops);
+
+            // Kick off the insert operation
+            TableOperation insertOperation = TableOperation.Insert(newCommand);
+            await commandQueueTable.ExecuteAsync(insertOperation);
+
+            return newCommand.OperationId;
         }
 
-        public Task<Guid> OrderAttack(Guid sessionId, Guid phaseId, Guid sourceRegion, string sourceRegionEtag, Guid targetRegion, uint numberOfTroops)
+        public async Task<Guid> Redeploy(Guid sessionId, Guid phaseId, String nationEtag, Guid sourceRegion, Guid targetRegion, UInt32 numberOfTroops)
         {
-            throw new NotImplementedException();
+            CloudTable commandQueueTable = GetCommandQueueTableForSession(sessionId);
+
+            // Create a new table entry
+            CommandQueueTableEntry newCommand = CommandQueueTableEntry.CreateRedeployMessage(sessionId, phaseId, String.Empty, sourceRegion, targetRegion, numberOfTroops);
+
+            // Kick off the insert operation
+            TableOperation insertOperation = TableOperation.Insert(newCommand);
+            await commandQueueTable.ExecuteAsync(insertOperation);
+
+            return newCommand.OperationId;
         }
 
-        public Task<Guid> Redeploy(Guid sessionId, Guid phaseId, string nationEtag, Guid sourceRegion, Guid targetRegion, uint numberOfTroops)
+        public async Task<IEnumerable<ICommandQueueMessage>> GetQueuedCommands(Guid sessionId)
         {
-            throw new NotImplementedException();
+            CloudTable commandQueueTable = GetCommandQueueTableForSession(sessionId);
+
+            List<CommandQueueTableEntry> results = new List<CommandQueueTableEntry>();
+            TableQuery<CommandQueueTableEntry> query = new TableQuery<CommandQueueTableEntry>()
+                .Where(TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, sessionId.ToString()));
+
+            // Initialize the continuation token to null to start from the beginning of the table.
+            TableContinuationToken continuationToken = null;
+
+            // Loop until the continuation token comes back as null
+            do
+            {
+                var queryResults = await commandQueueTable.ExecuteQuerySegmentedAsync(query, continuationToken);
+                continuationToken = queryResults.ContinuationToken;
+                results.AddRange(queryResults.Results);
+            }
+            while (continuationToken != null);
+
+            return results;
         }
 
-        public Task RemoveCommands(Guid sessionId, IEnumerable<Guid> operationIds)
+        public async Task RemoveCommands(Guid sessionId, IEnumerable<Guid> operationIds)
         {
-            throw new NotImplementedException();
+            CloudTable commandQueueTable = GetCommandQueueTableForSession(sessionId);
+            await commandQueueTable.DeleteAsync();
+        }
+
+        public CloudTable GetCommandQueueTableForSession(Guid sessionId)
+        {
+            String commandQueueTableName = String.Format(CommandQueueTableNameSyntax, sessionId.ToString().Replace("-", String.Empty));
+            CloudTable commandQueueTable = TableClient.GetTableReference(commandQueueTableName);
+            commandQueueTable.CreateIfNotExists();
+            return commandQueueTable;
         }
 
         private CloudStorageAccount StorageAccount { get; set; }
         private CloudTableClient TableClient { get; set; }
-        public CloudTable CommandQueueTable { get; set; }
     }
 }
---
+++ Peril.Api.Repository.Azure/Model/CommandQueueTableEntry.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using Peril.Api.Repository.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Peril.Api.Repository.Azure.Model
+{
+    public class CommandQueueTableEntry : TableEntity, IDeployReinforcementsMessage, IOrderAttackMessage, IRedeployMessage
+    {
+        static public CommandQueueTableEntry CreateReinforceMessage(Guid sessionId, Guid phaseId, Guid targetRegion, String targetRegionEtag, UInt32 numberOfTroops)
+        {
+            return new CommandQueueTableEntry(sessionId, Guid.NewGuid())
+            {
+                RawMessageType = (Int32)CommandQueueMessageType.Reinforce,
+                PhaseId = phaseId,
+                TargetRegion = targetRegion,
+                TargetRegionEtag = targetRegionEtag,
+                RawNumberOfTroops = (Int32)numberOfTroops
+            };
+        }
+
+        static public CommandQueueTableEntry CreateAttackMessage(Guid sessionId, Guid phaseId, Guid sourceRegion, String sourceRegionEtag, Guid targetRegion, UInt32 numberOfTroops)
+        {
+            return new CommandQueueTableEntry(sessionId, Guid.NewGuid())
+            {
+                RawMessageType = (Int32)CommandQueueMessageType.Attack,
+                PhaseId = phaseId,
+                SourceRegion = sourceRegion,
+                SourceRegionEtag = sourceRegionEtag,
+                TargetRegion = targetRegion,
+                RawNumberOfTroops = (Int32)numberOfTroops
+            };
+        }
+
+        static public CommandQueueTableEntry CreateRedeployMessage(Guid sessionId, Guid phaseId, String nationEtag, Guid sourceRegion, Guid targetRegion, UInt32 numberOfTroops)
+        {
+            return new CommandQueueTableEntry(sessionId, Guid.NewGuid())
+            {
+                RawMessageType = (Int32)CommandQueueMessageType.Redeploy,
+                PhaseId = phaseId,
+                SourceRegion = sourceRegion,
+                TargetRegion = targetRegion,
+                RawNumberOfTroops = (Int32)numberOfTroops
+            };
+        }
+
+        private CommandQueueTableEntry(Guid sessionId, Guid messageId)
+        {
+            PartitionKey = sessionId.ToString();
+            RowKey = messageId.ToString();
+        }
+
+        public CommandQueueTableEntry()
+        {
+
+        }
+
+        public Guid SessionId { get { return Guid.Parse(PartitionKey); } }
+        public Guid OperationId { get { return Guid.Parse(RowKey); } }
+        public CommandQueueMessageType MessageType { get { return (CommandQueueMessageType)RawMessageType; } }
+        public UInt32 NumberOfTroops { get { return (UInt32)NumberOfTroops; } }
+
+        public Guid PhaseId { get; set; }
+        public Guid TargetRegion { get; set; }
+        public String TargetRegionEtag { get; set; }
+        public Guid SourceRegion { get; set; }
+        public String SourceRegionEtag { get; set; }
+
+
+        public Int32 RawMessageType { get; set; }
+        public Int32 RawNumberOfTroops { get; set; }
+    }
+}
