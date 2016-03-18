e87e79afc6c2b65d0bf8da0694dbcbbba492cfc7
StephenE <sedevo-github@yahoo.co.uk>
2016-03-18 21:19:32 +0000

    Disable IntegrationTestStartGame_WithTwoUsers

--- Peril.Api.Tests/IntegrationTest.cs
+++ Peril.Api.Tests/IntegrationTest.cs
@@ -15,7 +15,7 @@ namespace Peril.Api.Tests
         [TestMethod]
         [TestCategory("Integration")]
         [DeploymentItem(@"Data\ValidWorldDefinition.xml", "WorldData")]
-        // [Ignore]
+        [Ignore]
         public async Task IntegrationTestStartGame_WithTwoUsers()
         {
             ControllerMock primaryUser = new ControllerMock();
