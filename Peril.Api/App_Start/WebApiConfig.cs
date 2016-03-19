21bb0ab814a360ffc7860838821bc6390dd80481
StephenE <sedevo-github@yahoo.co.uk>
2016-03-19 14:01:18 +0000

    Enabled CORS to help Anthony test

--- Peril.Api/App_Start/WebApiConfig.cs
+++ Peril.Api/App_Start/WebApiConfig.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
+﻿using Microsoft.Owin.Security.OAuth;
 using System.Web.Http;
-using Microsoft.Owin.Security.OAuth;
-using Newtonsoft.Json.Serialization;
+using System.Web.Http.Cors;
 
 namespace Peril.Api
 {
@@ -12,6 +8,11 @@ namespace Peril.Api
     {
         public static void Register(HttpConfiguration config)
         {
+            // Enable Cors
+            var defaultCors = new EnableCorsAttribute("beta.lairoftheraven.co.uk", "*", "*");
+            defaultCors.SupportsCredentials = true;
+            config.EnableCors(defaultCors);
+
             // Web API configuration and services
             // Configure Web API to use only bearer token authentication.
             config.SuppressDefaultHostAuthentication();
