e4bb5225e406c000fc1dea777bceec4eeb472f4d
StephenE <sedevo-github@yahoo.co.uk>
2017-02-05 20:47:05 +0000

    This seems like it would be important, but it's been a year since I made this change so who knows why

--- Peril.Api/Content/peril.js
+++ Peril.Api/Content/peril.js
@@ -5,14 +5,13 @@
     var interactionTarget = ";"
     var firstRun = true;
 
-    var apiUriBase = "http://localhost:24063/";
+    var apiUriBase = "http://devolan.azurewebsites.net/";
     var maxPlayers = 14;
     
     var currentGame = {};
     currentGame.GameId = "";
     currentGame.PhaseId = "";
     currentGame.PhaseType = null;
-    currentGame.OwnerId = "";
 
     var currentPlayers = {};
 
